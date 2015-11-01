08f3cf34dfe3a0a4c6890091db69ca49e2d5e6f0
Edgar Duran <edgarduran@Edgars-MacBook-Pro.local>
2015-10-31 19:21:50 -0600

    Delete numbers

--- spec/features/person_view_spec.rb
+++ spec/features/person_view_spec.rb
@@ -45,4 +45,11 @@ describe 'the person view', type: :feature do
     expect(page).to have_content('3034564544')
     expect(page).to_not have_content(old_number)
   end
+
+  it 'has links to delete each phone number' do
+    person.phone_numbers.each do |phone|
+      expect(page).to have_link('delete', href: phone_number_path(phone))
+    end
+  end
+
 end
